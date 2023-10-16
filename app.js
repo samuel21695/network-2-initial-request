69bdeea02b069b954f02302f6025fd535bde48b3
samuel21695 <samuel21695@gmail.com>
2023-10-16 14:34:30 +0900

    server listen

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ const server = http.createServer((req, res) =>  {
   console.log("URL로 최초접속하는 요청 유형 확이나기");
   console.log(req.method);
 })
+
+server.listen(3000, () => {
+  console.log(`server running : http://localhost:3000/`)
+})
