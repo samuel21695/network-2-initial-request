fe9f5b3112a76486593f635d99cb925edf981adb
samuel21695 <samuel21695@gmail.com>
2023-10-16 14:40:42 +0900

    check initial type of request

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
   def do_GET(self):
     print("URL로 최초접속하는 트랜잭션 확인하기")
     print(self.path)
+    print("URL로 최초접속하는 요청 유형 확인하기")
+    print(self.command)
+
