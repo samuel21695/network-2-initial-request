44252cafef0b4bfa01281bfacbf1395e01566960
samuel21695 <samuel21695@gmail.com>
2023-10-16 14:33:32 +0900

    check initial type of request

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const server = http.createServer((req, res) =>  {
 
   console.log("URL로 최초접속하는 트랜잭션 확인하기");
   console.log(req.url);
+  console.log("URL로 최초접속하는 요청 유형 확이나기");
+  console.log(req.method);
 })
