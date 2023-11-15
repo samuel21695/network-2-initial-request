501af19e97587ea96af407123181ed15371409e5
samuel21695 <samuel21695@gmail.com>
2023-11-15 10:58:34 +0900

    read file

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) =>  {
-
+  if (req.url === "/" && req.method === 'GET') {
+    fs.readFile('index.html', 'utf8', (err, data) => {
+      if (err) {
+        serverErrorLog();
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html'});
+      res.end(data);
+    })
+  }
   console.log("URL로 최초접속하는 트랜잭션 확인하기");
   console.log(req.url);
   console.log("URL로 최초접속하는 요청 유형 확인나기");
